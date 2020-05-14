3297f9ebfe8b831d624bdc3747954d1eb34b270c
misa198 <51451216+misa198@users.noreply.github.com>
2020-05-14 23:30:44 +0700

    update

---
+++ README.md
@@ -0,0 +1,40 @@
+# Herokuapp Awake
+
+Keep your herokuapp always awake
+
+## requirement
+* Node
+* Yarn or npm
+
+## Installation
+
+```bash
+# If you use npm
+$ npm i heroku-awake
+
+# If you use yarn
+$ yarn add heroku-awake
+```
+
+## Usage
+
+```javascript
+const express = require('express');
+const herokuAwake = require('heroku-awake');
+
+const PORT = 3000;
+const url = 'https://your-app.herokuapp.com';
+const time = 10;
+
+const app = express();
+
+app.listen(PORT, () => {
+  herokuAwake(
+    url,
+    time // Your application interval is refreshed, default is 25 mins
+  );
+});
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+
+const wakeUp = (url, interval = 25, callback) => {
+  const milliseconds = interval * 60000;
+  setTimeout(() => {
+    try {
+      console.log(`setTimeout called.`);
+      fetch(url).then(() => console.log(`Fetching ${url}.`));
+    } catch (err) {
+      console.log(`Error fetching ${url}: ${err.message} 
+            Will try again in ${interval} minutes...`);
+    } finally {
+      try {
+        callback();
+      } catch (e) {
+        callback ? console.log("Callback failed: ", e.message) : null;
+      } finally {
+        return wakeUpDyno(url, interval, callback);
+      }
+    }
+  }, milliseconds);
+};
+
+module.exports = wakeUp;
